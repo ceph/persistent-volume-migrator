214262b4359c80efa4b31137647a812852b295cb
Yug Gupta <yuggupta27@gmail.com>
2021-08-31 12:05:00 +0530

    internal: csi pvc bound
    converting flex volume to csi
    
    Co-authored-by: subhamkrai <srai@redhat.com>
    Signed-off-by: Yug Gupta <yuggupta27@gmail.com>

--- cmd/flexToCsi.go
+++ cmd/flexToCsi.go
@@ -78,56 +78,84 @@ func migrateFlexToCSI() {
 	if err != nil {
 		fail(fmt.Sprintf("failed to create kubernetes client %v\n", err))
 	}
+	fmt.Println("Yug Created Kubernetes Client")
+	fmt.Println()
 	// 2. List all the PVC from the source storageclass
 	pvcs, err := kubernetes.ListAllPVCWithStorageclass(client, sourceStorageClass)
 	if err != nil {
-		fail(fmt.Sprintf("failed to create kubernetes client %v\n", err))
+		fail(fmt.Sprintf("failed to list PVCs from the storageclass %v\n", err))
 	}
-
+	fmt.Println("Yug PVCs listed", pvcs)
+	fmt.Println()
+	fmt.Println()
 	if pvcs == nil || len(*pvcs) == 0 {
 		fail(fmt.Sprintf("no PVC found with storageclass %v\n", sourceStorageClass))
 	}
+	fmt.Println()
+	fmt.Println()
 	//  3. Change Reclaim policy from Delete to Reclaim
 	for _, p := range *pvcs {
+		fmt.Println("New PVC", p)
+		fmt.Println()
+		fmt.Println()
 		pv, err := kubernetes.GetPV(client, p.Spec.VolumeName)
 		if err != nil {
 			fmt.Printf("failed to get PV object with name %s \n", p.Spec.VolumeName)
 			continue
 		}
+		fmt.Println("Yug PV found", pv)
+		fmt.Println()
 		err = kubernetes.UpdateReclaimPolicy(client, pv)
 		if err != nil {
 			fmt.Printf("failed to update ReclaimPolicy for PV object %s \n", p.Spec.VolumeName)
 			continue
 		}
+		fmt.Println()
 		// 3. Retrive old ceph volume name from PV
 		rbdImageName := kubernetes.GetFlexVolumeName(pv)
 		if rbdImageName == "" {
 			fail("rbdImageName cannot be nil in Flex PV object")
 		}
+		fmt.Println("Yug rbdImageName found", rbdImageName)
+		fmt.Println()
 		// 4. Delete the PVC object
 		err = kubernetes.DeletePVC(client, &p)
 		if err != nil {
+			fmt.Println()
 			// TODO have an option to revert back if some Delete fails?
 			fmt.Printf("failed to Delete PVC object %s \n", p.Name)
 			continue
 		}
+		fmt.Println("Yug rbdImageName found", rbdImageName)
+		fmt.Println()
 		//5. Create new PVC with same name in destination storageclass
 		csiPVC := kubernetes.GenerateCSIPVC(destinationStorageClass, &p)
+		fmt.Println("Yug structure of new PVC with same name created in CSI", csiPVC)
+		fmt.Println()
 		// 6. Create new CSI PVC
 		csiPV, err := kubernetes.CreatePVC(client, csiPVC, 5)
+		if err != nil {
+			fmt.Println("failed to Create CSI PVC object", p.Name, err)
+			continue
+		}
+		fmt.Println("Yug new PVC with same name created in CSI. Persistent Volume: ", csiPV)
+		fmt.Println()
 		//6. Extract new volume name from CSI PV
 		csiRBDImageName := kubernetes.GetCSIVolumeName(csiPV)
 		if csiRBDImageName == "" {
 			fail("csiRBDImageName cannot be nil in PV object")
 		}
+		fmt.Println("Yug new volume name from CSI PV: ", csiRBDImageName)
 		clusterID := kubernetes.GetClusterID(csiPV)
 		if clusterID == "" {
 			fail("clusterID cannot be nil in PV object")
 		}
+		fmt.Println("Yug clusterID of csiPV ", clusterID)
 		poolName := kubernetes.GetCSIPoolName(csiPV)
 		if poolName == "" {
 			fail("poolName cannot be nil in PV object")
 		}
+		fmt.Println("Yug Poolname of csiPV ", poolName)
 		// TODO add support for datapool
 
 		csiConfig, err := kubernetes.GetCSIConfiguration(client, rookNamespace)
@@ -135,40 +163,67 @@ func migrateFlexToCSI() {
 			fmt.Printf("fail to get configmap %v\n", err)
 			continue
 		}
+		fmt.Println("Yug csiConfig ", csiConfig)
+		fmt.Println()
 		var monitor string
 		for _, c := range csiConfig {
 			if c.ClusterID == clusterID {
 				monitor = strings.Join(c.Monitors, ",")
 			}
 		}
+		fmt.Println("mon")
 		if monitor == "" {
 			fail(fmt.Sprintf("failed to get monitor information %v \n", csiConfig))
 		}
+		fmt.Println("YUG mon found", monitor)
 		user, key, err := kubernetes.GetRBDUserAndKeyFromSecret(client, cephClusterNamespace)
 		if err != nil {
+			fmt.Println("YUG err in GetRBDUserAndKeyFromSecret", err)
+			fmt.Println()
 			fail(err.Error())
 		}
-
+		fmt.Println()
+		fmt.Println("YUG NO err in GetRBDUserAndKeyFromSecret", err)
+		fmt.Println()
 		conn, err := rbd.NewConnection(monitor, user, key, poolName, "")
 		if err != nil {
+			fmt.Println("YUG err in NewConnection", err)
+			fmt.Println()
 			fail(err.Error())
 		}
+		fmt.Println("YUG NO err in NewConnection", err)
 		defer conn.Destroy()
 		//7. Delete the CSI volume in ceph cluster
 		err = conn.RemoveVolume(csiRBDImageName)
 		if err != nil {
+			fmt.Println("YUG err in RemoveVolume", err)
+			fmt.Println()
 			fail(err.Error())
 		}
+		fmt.Println()
+		fmt.Println("YUG NO err in RemoveVolume", err)
 		//  8. Rename old ceph volume to new CSI volume
 		err = conn.RenameVolume(csiRBDImageName, rbdImageName)
 		if err != nil {
+			fmt.Println("YUG err in RenameVolume", err)
+			fmt.Println()
 			fail(err.Error())
 		}
+		fmt.Println()
+		fmt.Println("YUG NO err in RenameVolume", err)
 		//  9. Delete old PV object
 		err = kubernetes.DeletePV(client, pv)
 		if err != nil {
+			fmt.Println("YUG err in DeletePV", err)
+			fmt.Println()
 			fail(err.Error())
 		}
+		fmt.Println()
+		fmt.Println("YUG NO err in DeletePV", err)
+		fmt.Println()
 	}
+	fmt.Println()
+	fmt.Println("YUG Done with all PVC")
+	fmt.Println()
 	fmt.Printf("successfully migrated all the PVC from FlexVolume to CSI")
 }
