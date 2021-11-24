e8d0fbb5ae5ad16d051084373dd5b736289b8f92
subhamkrai <srai@redhat.com>
2021-11-24 10:22:57 +0530

    core: change cli arg name for consistency and readability
    change cli arg name for consistency and readability
    
    Signed-off-by: subhamkrai <srai@redhat.com>

--- tests/script/github_action_helper.sh
+++ tests/script/github_action_helper.sh
@@ -58,7 +58,7 @@ test_flex_migration_for_all_pvc(){
   go build main.go
   MIGRATION_POD=$(kubectl -n rook-ceph get pod -l app=rook-ceph-migrator -o jsonpath='{.items[*].metadata.name}')
   kubectl -n rook-ceph cp main "$MIGRATION_POD":/root/
-  kubectl -n rook-ceph exec -it "$MIGRATION_POD" -- sh -c "cd root/ && ./main --sourcestorageclass=rook-ceph-block --destinationstorageclass=csi-rook-ceph-block"
+  kubectl -n rook-ceph exec -it "$MIGRATION_POD" -- sh -c "cd root/ && ./main --source-sc=rook-ceph-block --destination-sc=csi-rook-ceph-block"
   exit_code_of_last_command=$?
   if [ $exit_code_of_last_command -ne 0 ]; then
     echo "Exit code migration command is non-zero $exit_code_of_last_command. Migration failed"
@@ -73,7 +73,7 @@ test_flex_migration_for_single_pvc(){
   go build main.go
   MIGRATION_POD=$(kubectl -n rook-ceph get pod -l app=rook-ceph-migrator -o jsonpath='{.items[*].metadata.name}')
   kubectl -n rook-ceph cp main "$MIGRATION_POD":/root/
-  kubectl -n rook-ceph exec -it "$MIGRATION_POD" -- sh -c "cd root/ && ./main --pvc=rbd-pvc --pvc-namespace=default --destinationstorageclass=csi-rook-ceph-block"
+  kubectl -n rook-ceph exec -it "$MIGRATION_POD" -- sh -c "cd root/ && ./main --pvc=rbd-pvc --pvc-ns=default --destination-sc=csi-rook-ceph-block"
   kubectl create -f https://raw.githubusercontent.com/rook/rook/release-1.7/cluster/examples/kubernetes/ceph/csi/rbd/pod.yaml
   wait_for_sample_pod_to_be_ready_state
   verify_file_data_and_file_data
